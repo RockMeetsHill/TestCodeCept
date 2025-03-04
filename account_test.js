048b15ae3f81025d0fe4471af428a2b616e1b0e4
nhatt <nhattran2016.nt@gmail.com>
2025-03-04 09:56:32 +0700

    comitted wrong , old file , new file with new tests

--- account_test.js
+++ account_test.js
@@ -1,6 +1,84 @@
 Feature('account');
 
-Scenario('test something',  ({ I }) => {
+Scenario('test get all accounts from api',  ({ I }) => {
     I.sendGetRequest('/Account');
     I.seeResponseCodeIsSuccessful();
 });
+
+Scenario('test create new account to api',  ({ I }) => {
+    //if the api call requires a token
+    //I.amBearerAuthenticated(secret('token-is-here'));
+    
+    //to use with custom authorization with headers use:
+    //I.haveRequestHeaders method
+
+    const response =  I.sendPostRequest('/Account', {
+        accName: "ccjss",
+        accPass: "1234",
+        accEmail: "ccjss@api.com",
+        dob: "2025-03-03T17:42:46.049Z",
+        gender: true,
+        role: "Manager"
+      });
+
+      I.seeResponseEquals(200);
+});
+
+
+
+Scenario('delete user by id', ({ I }) => {
+    
+    const resp = I.sendDeleteRequest('/Account/?id=1003');
+    I.seeResponseCodeIsSuccessful();
+  
+    
+});
+
+Scenario('get user by query', ({ I }) => {
+    
+    const resp = I.sendGetRequest('/Account/id?id=1003');
+    I.seeResponseCodeIsSuccessful();
+  
+    
+    I.seeResponseContainsKeys(['data']);
+
+    // check data for partial inclusion
+    I.seeResponseContainsJson({
+      data: {
+        accId: 1003,
+        accName: "ccjs",
+        accPass: "123",
+        accEmail: "ccjs@api.com",
+        dob: "2025-03-03T17:42:46.05",
+        gender: true,
+        role: "Manager",
+        isActivated: true,
+        isApproved: true        
+      },
+    });
+});
+
+Scenario('test create duplicate account to api',  ({ I }) => {
+    //checking create duplicate account : return body will have "success: false
+//                                                                  "
+
+    const response =  I.sendPostRequest('/Account', {
+        accName: "ccjss",
+        accPass: "1234",
+        accEmail: "ccjss@api.com",
+        dob: "2025-03-03T17:42:46.049Z",
+        gender: true,
+        role: "Manager"
+      });
+
+      I.seeResponseContainsKeys(['success']);
+
+      I.seeResponseContainsJson({
+        success: false
+
+      })
+
+});
+
+
+
